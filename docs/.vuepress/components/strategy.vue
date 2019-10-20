e685d737df05e7864d5d03c4611fdb044275c7a3
xuwenchao <842390367@qq.com>
2019-10-20 22:27:21 +0800

    add Animate for strategy pattern

--- docs/.vuepress/components/strategy.vue
+++ docs/.vuepress/components/strategy.vue
@@ -1,9 +1,33 @@
 <template>
+  <div class="wrapper">
+    <div class="strategy-ball"></div>
+  </div>
 </template>
 
 <script>
-import '@js/strategy.js'
+import { Animate } from '@js/strategy.js'
 export default {
-  name: 'strategy'
+  name: 'strategy',
+  mounted() {
+    const animate = new Animate(document.querySelector('.strategy-ball'))
+    animate.move(100, 'slow').move(100, 'fast').move(100, 'slow')
+  }
 }
 </script>
+
+<style scoped>
+.wrapper {
+  position: relative;
+  width: 100%;
+  height: 300px;
+  border: 1px solid;
+}
+.strategy-ball {
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  background: rgb(9, 209, 166);
+  border-radius: 50%;
+  cursor: pointer;
+}
+</style>
--- docs/js/strategy.js
+++ docs/js/strategy.js
@@ -51,8 +51,8 @@ const moneyS = bonusS.getBonus()
 console.log(moneyS)
 
 /** 
- * JavaScript对象版本
-*/
+ * JavaScript对象版
+ */
 const strategies = {
   A: salary => salary * 3,
   B: salary => salary * 2
@@ -60,3 +60,63 @@ const strategies = {
 const calculateBouns = (performance, salary) => strategies[performance](salary)
 const bonusA = calculateBouns('A', 10000)
 console.log(bonusA)
+
+/**
+ * 使用策略模式实现动画
+ */
+class MoveSlow {
+  speed = 1
+  onEnd() {
+    console.log('move slowly done')
+  }
+}
+
+class MoveFast {
+  speed = 5
+  onEnd() {
+    console.log('move fast done')
+  }
+}
+
+const Animate = function(dom) {
+  this.dom = dom
+  this.queue = []
+  this.target = 0
+  this.movement = null
+  this.runing = false
+}
+
+// 开始移动
+Animate.prototype.move = function(distance, speed = 'slow') {
+  if (this.runing) {
+    this.queue.push(() => this.move(distance, speed))
+  } else {
+    this.runing = true
+    this.movement = speed === 'slow' ? new MoveSlow() : new MoveFast()
+    this.curPos = parseInt(this.dom.style.left) || 0
+    this.target = this.curPos + distance
+    this.step()
+  }
+  console.log(this.queue)
+  return this
+}
+
+// 移动中每一步要做的事情
+Animate.prototype.step = function() {
+  this.curPos += this.movement.speed
+  this.dom.style.left = `${this.curPos}px`
+  if (this.target > this.curPos) {
+    requestAnimationFrame(() => this.step())
+  } else {
+    this.movement.onEnd()
+    this.end()
+  }
+}
+
+// 移动结束，出栈&开始下一步
+Animate.prototype.end = function() {
+  this.runing = false
+  this.queue[0] && this.queue.shift()()
+}
+
+export { Animate }
