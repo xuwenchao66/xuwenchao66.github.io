3c3fd58fcab4b2e6f62c6559a6d657cfe9c742a7
xuwenchao <842390367@qq.com>
2019-11-02 16:25:39 +0800

    add createProxyCacheFactory for proxy pattern

--- docs/js/proxy.js
+++ docs/js/proxy.js
@@ -1,5 +1,7 @@
 /**
- * 使用代理模式实现图片加载占位符
+ * 虚拟代理
+ * 
+ * 实现图片预加载
  */
 import imgHoder from '../img/holder.svg'
 
@@ -17,5 +19,36 @@ const loadImgWithHolder = (() => {
     img.src = src
   }
 })()
+
+
+/**
+ * 缓存代理
+ * 
+ * 缓存计算结果
+ */
+
+// 假装是复杂的计算
+function mult() {
+  const args = Array.prototype.slice.apply(arguments)
+  return args.reduce((acc, item) => acc * item, 1)
+}
+
+// 创建缓存代理的工厂函数，缓存计算过的结果
+const createProxyCacheFactory = (fn) => {
+  const cache = {}
+  return function() {
+    const args = Array.prototype.join.call(arguments, ',')
+    if (cache[args]) {
+      console.log('return value from cahce')
+      return cache[args]
+    }
+    return cache[args] = fn.apply(this, arguments)
+  }
+}
+
+const proxyMult = createProxyCacheFactory(mult)
+
+console.log(proxyMult(1, 2, 3, 4))
+console.log(proxyMult(1, 2, 3, 4))
 
 export { loadImg, loadImgWithHolder }
