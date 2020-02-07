08d00c03178ffd4fd5b877213dfef6e266d12738
xuwenchao <842390367@qq.com>
2020-02-07 17:48:37 +0800

    adjust folder structure

---
+++ docs/.vuepress/nav.js
@@ -0,0 +1,14 @@
+module.exports = [{
+  text: '关于我',
+  link: '/'
+},
+{
+  text: '设计模式',
+  link: '/designPatterns/'
+}, {
+  text: '算法',
+  link: '/arithmetic/'
+}, {
+  text: '源码解析',
+  link: '/sourceCode/'
+}]
---
+++ docs/.vuepress/sidebar.js
@@ -0,0 +1,22 @@
+module.exports = {
+  '/designPatterns/': [
+    '',
+    'singleton',
+    'strategy',
+    'proxy',
+    'iterator',
+    'observer',
+    'command',
+    'composite',
+    'templateMethod',
+    'flyweight',
+    'chainOfResponsibility',
+    'mediator',
+    'decorator',
+    'state',
+    'adapter'
+  ],
+  '/sourceCode/': [
+    ''
+  ]
+}
