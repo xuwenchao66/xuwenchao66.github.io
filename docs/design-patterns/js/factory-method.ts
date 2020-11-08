6847c048e45245ecd77c5c20cdabad8de57f9f8e
xuwenchao <842390367@qq.com>
2020-11-08 16:54:12 +0800

    finish factory-method.

--- docs/design-patterns/js/factory-method.ts
+++ docs/design-patterns/js/factory-method.ts
@@ -1,1 +1,48 @@
-1
+const platforms = {
+  android: 'android',
+  ios: 'ios'
+}
+
+// 抽象产品（接口）
+interface IButton {
+  render(): void
+}
+// 抽象工厂
+abstract class DialogFactory {
+  abstract createButton(): IButton
+  abstract platform: string
+  render() {
+    console.log(`Render on ${this.platform}.`)
+    const button = this.createButton()
+    button.render()
+  }
+}
+// 具体产品
+class IOSButton implements IButton {
+  render() {
+    console.log('Rendering ios button.')
+  }
+}
+// 具体产品
+class AndroidButton implements IButton {
+  render() {
+    console.log('Rendering android button.')
+  }
+}
+// 具体工厂
+class IOSDialogFactory extends DialogFactory {
+  platform = platforms.ios
+  createButton(): IButton {
+    return new IOSButton()
+  }
+}
+// 具体工厂
+class AndroidDialogFactory extends DialogFactory {
+  platform = platforms.android
+  createButton(): IButton {
+    return new AndroidButton()
+  }
+}
+
+new IOSDialogFactory().render()
+new AndroidDialogFactory().render()
