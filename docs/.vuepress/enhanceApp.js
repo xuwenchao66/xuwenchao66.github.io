8de4694ab8b0cd2daabbea65a1c67837a5380aa6
xuwenchao <842390367@qq.com>
2023-11-26 18:15:20 +0800

    feat: 🎸 about me

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,13 +1,14 @@
-import "./style.css";
+import './style.css'
 
 const redirectList = {
-  "/": "/blogs/ast-and-babel/",
-  "/reading-notes/": "/reading-notes/refactoring/",
-};
+  '/': '/blogs/ast-and-babel/',
+  '/blogs/': '/blogs/ast-and-babel/',
+  '/reading-notes/': '/reading-notes/refactoring/'
+}
 
 export default ({ router }) => {
   router.beforeEach((to, from, next) => {
-    const redirectPath = redirectList[to.path];
-    redirectPath ? next({ path: redirectPath }) : next();
-  });
-};
+    const redirectPath = redirectList[to.path]
+    redirectPath ? next({ path: redirectPath }) : next()
+  })
+}
